6abc8af3be78a2de24012de6b8651f85cacb9bc7
aviv57 <aviv57@gmail.com>
2025-05-05 08:01:00 +0300

    Update answers.js

--- src/data/answers.js
+++ src/data/answers.js
@@ -2,6 +2,14 @@ import answers_base from './answers_base'
 
 const answers = [...answers_base,
   'בטוחה',
+  'מדורה',
+  'מטייל',
+  'התחלה',
+  'ניווט',
+  'חומצה',
+  'סביבה',
+  'חידון',
+  'הפסקה',    
 ]
 
 export default answers
